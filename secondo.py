ea2889e0523b78dfecf06792bc47ac0684c96465
christianCITS <christian.cioetaa@gmail.com>
2024-02-14 18:31:45 +0100

    aaa

---
+++ secondo.py
@@ -0,0 +1,24 @@
+i1= 20
+i2=40
+s1="ciao"
+s2="buongiorno"
+f1=10.5
+f2=20.5
+b1=True
+b2=False
+
+print(i1+i2)
+print(s1+s2)
+print(f1+f2)
+print(b1+b2)
+print(i1+s1)
+print(s1+i2)
+print(i1+f2)
+print(f2+i2)
+print(i1+b1)
+print(f1+b1)
+print(s1+b1)
+print(b1+i1)
+print(b1+s1)
+print(b1+f1)
+
