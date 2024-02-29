7ab55bdd6ee976365c05379cedfb18184e2f17e2
christianCITS <christian.cioetaa@gmail.com>
2024-02-29 17:38:49 +0100

    quart p1

---
+++ quarto29_02.py
@@ -0,0 +1,18 @@
+print(10/5) #divisione con risultato decimale
+print(10//5) #divisione senza decimali
+
+lista=[2,57,39,1,1,11,1,1]
+print (lista)
+lista.append(100) #aggiungo 100 alla fine della lista
+
+print(lista)
+
+lista[2]=10 #cambio il valore dellelemento numero 2 della lista ( contando da 0 sarebbe il terzo)
+print(lista)
+
+for v in lista: #stampa tutti gli elementi della lista in elenco (la variabile v potrebbe essere chiamata come si vuole)
+    print (v)
+
+    
+
+    
