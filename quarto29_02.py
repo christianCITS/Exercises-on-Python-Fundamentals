86181670884ef22b14bee108a38158e328e2bf48
christianCITS <christian.cioetaa@gmail.com>
2024-02-29 18:06:14 +0100

    N

--- quarto29_02.py
+++ quarto29_02.py
@@ -14,5 +14,22 @@ for v in lista: #stampa tutti gli elementi della lista in elenco (la variabile v
     print (v)
 
     
+#STAMPARE TABELLINA 23
+    
+t=23 #inizzializzo la variabile
+for i in range(11):#in questo modo la i del for sta per le volte che viene ripetuta l'operazione ovvero 23*1,23*2,23*3 fino a 10
+                   #ovvero il RANGE che gli abbiamo assegnato sopra(sarebbe 11 poiche iniziando a contare da 0 viene ripetuto 10 volte)
+    print(i*23)#stampo il risultato di i che in questo caso aumenta ogni volta di 1 (quindi 1,2,3,4,5,6,7,8,9,10) grazie al range per 10 e lo moltiplico per il primo numero della tabellina che voglio
 
     
+    
+
+    
+    
+
+
+
+
+
+
+
